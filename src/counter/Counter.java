ba527192ffd028a29c68cc064ce4c5c5b8083ba6
Alexandre Keruzec <alexandre.keruzec@gmail.com>
2018-04-11 14:12:45 +0200

    added globalCounter

--- src/counter/Counter.java
+++ src/counter/Counter.java
@@ -2,26 +2,38 @@ package counter;
 
 public class Counter {
 	private int value;
-	private int increment;
+	private final int increment;
+	private static int globalCounter = -1;
 	
 	public Counter() {
 		super();
 		this.value = 0;
 		this.increment = 1;
+		if(globalCounter==-1) {
+			globalCounter = 0;
+		}
 	}
 	
 	public Counter(int value, int increment) {
 		super();
 		this.value = value;
 		this.increment = increment;
+		if(globalCounter!=-1) {
+			globalCounter = 0;
+		}
 	}
 
 	public int getValue() {
 		return value;
 	}
 	
+	public static int getglobalCounter() {
+		return globalCounter;
+	}
+	
 	public void inc() {
 		value += increment;
+		globalCounter += increment;
 	}
 	
 	public String toString() {
--- src/counter/CounterTest.java
+++ src/counter/CounterTest.java
@@ -5,6 +5,9 @@ public class CounterTest {
 	public static void main(String[] args) {
 		Counter counter = new Counter();
 		Counter counter2 = new Counter(0, 2);
+		Counter counter3 = counter2;
+		
+		System.out.println("globalCounter : " + Counter.getglobalCounter());
 		
 		System.out.println(counter.getValue());
 		counter.inc();
@@ -18,7 +21,11 @@ public class CounterTest {
 		System.out.println(counter.toString());
 		System.out.println("counter equals counter 2 : " + counter.equals(counter2));
 		
+		System.out.println("counter 2 equals counter 3 : " + counter2.equals(counter3));
+		counter3.inc();
+		System.out.println(counter2.toString());		
 		
+		System.out.println("globalCounter : " + Counter.getglobalCounter());
 	}
 
 }
