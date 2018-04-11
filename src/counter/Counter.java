ec67921a2c6f4080e74047438568b5792d01bc0e
Alexandre Keruzec <alexandre.keruzec@gmail.com>
2018-04-11 11:04:32 +0200

    Counter constructor, getValue and inc

---
+++ src/counter/Counter.java
@@ -0,0 +1,20 @@
+package counter;
+
+public class Counter {
+	int value;
+	int increment;
+	
+	public Counter() {
+		super();
+		this.value = 0;
+		this.increment = 1;
+	}
+	
+	public Counter(int value, int increment) {
+		super();
+		this.value = value;
+		this.increment = increment;
+	}
+	
+	
+}
